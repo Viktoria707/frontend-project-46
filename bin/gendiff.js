403ff1c63d376d9bbb7c71ec614e2eac556b6989
Viktoriya <viktoriya_trof@mail.ru>
2024-12-20 13:13:25 +0300

    changed gendiff

--- bin/gendiff.js
+++ bin/gendiff.js
@@ -7,6 +7,8 @@ const program = new Command();
 const version = '1.0.0';
 program
     .version(version)
-    .description('Compares two configuration files and shows a difference.');
+    .description('Compares two configuration files and shows a difference.')
+    .arguments('<filepath1> <filepath2>')
+    .option('-f, --format [type]', 'output format');
 
 program.parse(process.argv);
