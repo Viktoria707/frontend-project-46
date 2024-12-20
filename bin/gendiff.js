e9eec310559c7522aa5ddd84a638c647d91f37de
Viktoriya <viktoriya_trof@mail.ru>
2024-12-20 14:40:16 +0300

    parser

--- bin/gendiff.js
+++ bin/gendiff.js
@@ -1,6 +1,8 @@
 #!/usr/bin/env node
 
 import { Command } from 'commander';
+import { parseJsonFile } from '../fileParser.js';
+import path from 'path';
 
 const program = new Command();
 
@@ -10,5 +12,18 @@ program
     .description('Compares two configuration files and shows a difference.')
     .arguments('<filepath1> <filepath2>')
     .option('-f, --format [type]', 'output format');
+
+program.action((filepath1, filepath2) => {
+    const absolutePath1 = path.resolve(filepath1);
+    const absolutePath2 = path.resolve(filepath2);
+    try {
+        const data1 = parseJsonFile(absolutePath1);
+        const data2 = parseJsonFile(absolutePath2);
+        console.log('Data from file1:', data1);
+        console.log('Data from file2:', data2);
+    } catch (error) {
+        console.error('Error reading files: ${error.message}');
+    }
+});
 
 program.parse(process.argv);
---
+++ fileParser.js
@@ -0,0 +1,6 @@
+import fs from 'fs';
+
+export const parseJsonFile = (filepath) => {
+    const data = fs.readFileSync(filepath, 'utf-8');
+    return JSON.parse(data);
+};
