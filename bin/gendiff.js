15f14bc0f5ab447ff5a4cde4edd66194998747be
Viktoriya <viktoriya_trof@mail.ru>
2025-01-07 20:22:48 +0300

    fixing the check 1

--- bin/gendiff.js
+++ bin/gendiff.js
@@ -1,27 +1,7 @@
 #!/usr/bin/env node
 
 import { program } from 'commander';
-
-import fs from 'fs';
-import path from 'path';
-import parse from '../src/fileParser.js';
-import getDifferences from '../src/genDifference.js';
-import formatChoice from '../src/formatters/index.js';
-
-const getContentParse = (filepath) => {
-  const normalizePath = path.resolve(process.cwd(), filepath);
-  const getContent = fs.readFileSync(normalizePath, 'utf-8');
-  const getExtension = path.extname(filepath).slice(1);
-
-  return parse(getContent, getExtension);
-};
-
-const genDiff = (filepath1, filepath2, formatName = 'stylish') => {
-  const data1 = getContentParse(filepath1);
-  const data2 = getContentParse(filepath2);
-
-  return formatChoice(getDifferences(data1, data2), formatName);
-};
+import genDiff from '../src/index.js';
 
 program
   .name('gendiff')
