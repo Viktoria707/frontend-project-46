4efb709b5b7e360af1a16bcb591a706543e894ed
Viktoriya <viktoriya_trof@mail.ru>
2025-01-08 22:29:00 +0300

    fixing the check 2

--- __tests__/genDiff.test.js
+++ __tests__/genDiff.test.js
@@ -1,4 +1,5 @@
 import fs from 'fs';
+import { fileURLToPath } from 'url';
 import path from 'path';
 import yaml from 'js-yaml';
 import getDifferences from '../src/genDifference.js';
@@ -6,7 +7,8 @@ import formatStylish from '../src/formatters/stylish.js';
 import formatPlain from '../src/formatters/plan.js';
 import formatJson from '../src/formatters/json.js';
 
-const __dirname = path.dirname(new URL(import.meta.url).pathname);
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = path.dirname(__filename);
 
 const getFixturePath = (filename) => path.join(__dirname, '../__fixtures__', filename);
 
--- eslint.config.js
+++ eslint.config.js
@@ -9,11 +9,36 @@ import pluginJs from '@eslint/js';
 const __filename = fileURLToPath(import.meta.url);
 const __dirname = path.dirname(__filename);
 const compat = new FlatCompat(
-  { baseDirectory: __dirname,
-  recommendedConfig: pluginJs.configs.recommended }
+  {
+    baseDirectory: __dirname,
+    recommendedConfig: pluginJs.configs.recommended 
+  }
 );
 
 export default [
-  {languageOptions: { globals: globals.node }},
-  ...compat.extends("airbnb"),
+  { 
+    languageOptions: { 
+      globals: { 
+        ...globals.node,
+        ...globals.jest
+      }
+    }
+  },
+  ...compat.extends('airbnb'),
+  {
+    plugins: {
+      jest: {
+        rules: {
+          'jest/consistent-test-it': ['error', { fn: 'test' }],
+          'jest/no-identical-title': 'error',
+          'jest/no-focused-tests': 'error',
+          'jest/no-test-prefixes': 'error',
+          'jest/no-done-callback': 'warn',
+          'jest/valid-expect': 'error',
+          'jest/valid-expect-in-promise': 'warn',
+          'jest/no-duplicate-hooks': 'error',
+        }
+      }
+    }
+  }
 ];
